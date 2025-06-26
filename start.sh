89d729cdc125f002a05f1798bfc6327122b11b86
Nick Sanford <nick.sanford@viam.com>
2025-06-26 15:34:35 -0400

    wip

--- start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# gdb --batch --ex run --ex bt --ex q --args /home/user/code/orbbec/build-conan/build/RelWithDebInfo/orbbec-module $@
-/home/user/code/orbbec/build-conan/build/RelWithDebInfo/orbbec-module $@
