d0aab27a4be7a07c123be611c9da6e8baf0a0a7b
Lia Stratopoulos <167905060+lia-viam@users.noreply.github.com>
2025-08-06 11:13:56 -0400

    remove install udev rules

--- src/cmake/install_udev_rules.cmake
+++
@@ -1,8 +0,0 @@
-add_custom_command(install_udev_rules
-    COMMAND
-        ${CMAKE_COMMAND} -E copy_if_different
-            "${CMAKE_CURRENT_BINARY_DIR}/_deps/orbbec-sdk-release-src/shared/99-obsensor-libusb.rules"
-            "/etc/udev/rules.d/"
-    COMMAND udevadm control --reload-rules
-    COMMAND udevadm trigger
-)
