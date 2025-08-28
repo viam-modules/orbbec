1e545c6bebbfa7136c6c466d76bbcb7a91259cc1
oliviamiller <106617921+oliviamiller@users.noreply.github.com>
2025-08-28 16:04:40 -0400

    fix path

--- bin/setup.bat
+++ bin/setup.bat
@@ -1,2 +1,2 @@
 @echo off
-Powershell.exe -ExecutionPolicy Bypass -Command "$env:ORBBEC_SDK_VERSION='%ORBBEC_SDK_VERSION%'; $env:ORBBEC_SDK_DIR='%ORBBEC_SDK_DIR%'; .\setup.ps1; exit $LASTEXITCODE"
+Powershell.exe -ExecutionPolicy Bypass -Command "$env:ORBBEC_SDK_VERSION='%ORBBEC_SDK_VERSION%'; $env:ORBBEC_SDK_DIR='%ORBBEC_SDK_DIR%'; bin\setup.ps1; exit $LASTEXITCODE"
