66f85a8840d20831668691979800fa3cd21e6a7f
oliviamiller <106617921+oliviamiller@users.noreply.github.com>
2025-09-02 15:33:34 -0400

    print ex

--- src/module/orbbec.cpp
+++ src/module/orbbec.cpp
@@ -926,10 +926,10 @@ void registerDevice(std::string serialNumber, std::shared_ptr<ob::Device> dev) {
 
             // RegCloseKey(hKey);
             // return true;
-    } catch (...) {
-        throw std::runtime_error("GOT EXCEPTION!");
-        //VIAM_SDK_LOG(error) << "error in windows " << e.what();
-       // throw std::runtime_error("error" + std::string(e.what()));
+    } catch (const std::exception& e) {
+        //throw std::runtime_error("GOT EXCEPTION!");
+        VIAM_SDK_LOG(error) << "error in windows " << e.what();
+        throw std::runtime_error("EXCEPTION" + std::string(e.what()));
     }
 #endif
     {
