8039f1a39dfda379493ddf29eb5f018865388afb
oliviamiller <106617921+oliviamiller@users.noreply.github.com>
2025-09-03 13:41:06 -0400

    lint

--- src/module/orbbec.cpp
+++ src/module/orbbec.cpp
@@ -852,46 +852,38 @@ void registerDevice(std::string serialNumber, std::shared_ptr<ob::Device> dev) {
 
     pointCloudFilter->setCreatePointFormat(OB_FORMAT_RGB_POINT);
 
-
 #ifdef _WIN32
-     // Command to set PowerShell execution policy to obtain metadata
-     //TODO: may have to run as admin
+    // On windows, we must add a metadata value to the windows device registry for the device to work correctly.
     try {
-    const char* command = "powershell -Command \"Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force\"";
-    int result = std::system(command);
-    if(result != 0) {
-        // command failed, try the backup
-        command = "powershell -Command \"Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser -Force\"";
+        const char* command = "powershell -Command \"Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force\"";
         int result = std::system(command);
         if (result != 0) {
-            VIAM_SDK_LOG(error) << "Could not set execution policy";
+            // command failed, try the backup
+            command = "powershell -Command \"Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser -Force\"";
+            int result = std::system(command);
+            if (result != 0) {
+                VIAM_SDK_LOG(error) << "Could not set execution policy";
+            }
         }
-    }
 
         // Base registry paths
         std::vector<std::string> searchTrees = {
             // KSCATEGORY_CAPTURE class,used for video capture devices
             "SYSTEM\\CurrentControlSet\\Control\\DeviceClasses\\{e5323777-f976-4f5b-9b55-b94699c46e44}",
-            //KSCATEGORY_RENDER class, used for rendering deviced
-            "SYSTEM\\CurrentControlSet\\Control\\DeviceClasses\\{65E8773D-8F56-11D0-A3B9-00A0C9223196}"
-        };
+            // KSCATEGORY_RENDER class, used for rendering devices
+            "SYSTEM\\CurrentControlSet\\Control\\DeviceClasses\\{65E8773D-8F56-11D0-A3B9-00A0C9223196}"};
 
         uint16_t vid = dev->getDeviceInfo()->vid();
         uint16_t pid = dev->getDeviceInfo()->pid();
-
-        std::string baseDeviceId = "##?#USB#VID_" + intToHex(vid) + "&PID_" + intToHex(pid);
+        s std::string baseDeviceId = "##?#USB#VID_" + intToHex(vid) + "&PID_" + intToHex(pid);
 
         for (const auto& subtree : searchTrees) {
-            std::cout << "\nProcessing Registry branch: " << subtree << "\n";
-            HKEY hkey;
-
             // Open the device registry key
+            HKEY hkey;
             if (RegOpenKeyExA(HKEY_LOCAL_MACHINE, subtree.c_str(), 0, KEY_ENUMERATE_SUB_KEYS, &hkey) != ERROR_SUCCESS) {
-                VIAM_SDK_LOG(error) << "Could not open subtree: " << subtree;
+                VIAM_SDK_LOG(error) << "Could not open device registry key: " << subtree;
                 continue;
             }
-
-            VIAM_SDK_LOG(info) << "successfully opened the key: " << subtree;
 
             char name[512];
             DWORD nameSize;
@@ -900,72 +892,58 @@ void registerDevice(std::string serialNumber, std::shared_ptr<ob::Device> dev) {
                 // reset before each call
                 nameSize = sizeof(name);
 
-                // enumerate all of the keys in the reg folder
+                // enumerate all of the keys in the devices folder we previously opened
                 LONG result = RegEnumKeyExA(hkey, index, name, &nameSize, NULL, NULL, NULL, NULL);
                 if (result == ERROR_NO_MORE_ITEMS) {
                     break;  // normal end of enumeration
                 } else if (result != ERROR_SUCCESS) {
-                    VIAM_SDK_LOG(error) << "Enumeration failed: " << result;
+                    VIAM_SDK_LOG(error) << "device registry key enumeration failed: " << result;
                     break;
                 }
                 std::string subKeyName(name);
+                // find the enteries for our orbbec device.
                 if (subKeyName.find("USB#VID_" + intToHex(vid) + "&PID_" + intToHex(pid)) == std::string::npos) {
                     // not a match for an orbbec device, go to next key
                     ++index;
                     continue;
                 }
-                std::cout << "Matched device: " << subKeyName << "\n";
+
                 std::string deviceParamsKey = subtree + "\\" + subKeyName + "\\#GLOBAL\\Device Parameters";
                 std::string valueName = "MetadataBufferSizeInKB0";
                 HKEY hDeviceKey;
-                // open the orbbec device parameters key (folder)
-                result = RegOpenKeyExA(
-                    HKEY_LOCAL_MACHINE,
-                    deviceParamsKey.c_str(),
-                    0,
-                    KEY_READ | KEY_SET_VALUE,
-                    &hDeviceKey
-                );
+                // open the orbbec device parameters key
+                result = RegOpenKeyExA(HKEY_LOCAL_MACHINE, deviceParamsKey.c_str(), 0, KEY_READ | KEY_SET_VALUE, &hDeviceKey);
                 if (result != ERROR_SUCCESS) {
-                    VIAM_SDK_LOG(error) << "Couldn't open device parameters key: " << deviceParamsKey;
+                    VIAM_SDK_LOG(error) << "Couldn't open windows registry device parameters key: " << deviceParamsKey;
                     ++index;
                     continue;
                 }
-                // check if the value metadata exists
+                // check if the metadata value already exists
                 DWORD data;
                 DWORD dataSize = sizeof(data);
-                // find the value of the bufferSize if it exists
                 result = RegQueryValueExA(hDeviceKey, valueName.c_str(), nullptr, nullptr, reinterpret_cast<BYTE*>(&data), &dataSize);
                 if (result == ERROR_FILE_NOT_FOUND) {
-                        // value does not exist yet, create it
-                        DWORD value = 5;
-                        result = RegSetValueExA(
-                            hDeviceKey,
-                            valueName.c_str(),
-                            0,
-                            REG_DWORD,
-                            reinterpret_cast<const BYTE*>(&value),
-                            sizeof(value)
-                        );
-                        if (result != ERROR_SUCCESS) {
-                            VIAM_SDK_LOG(error) << "Couldn't set metadata registry value for key " << deviceParamsKey;
-                        } else {
-                            VIAM_SDK_LOG(info) << "Created value " << valueName << " = " << value << " for key " << deviceParamsKey;
-                        }
+                    // value does not exist yet, create it.
+                    DWORD value = 5;
+                    result =
+                        RegSetValueExA(hDeviceKey, valueName.c_str(), 0, REG_DWORD, reinterpret_cast<const BYTE*>(&value), sizeof(value));
+                    if (result != ERROR_SUCCESS) {
+                        VIAM_SDK_LOG(error) << "Couldn't set metadata registry value for key " << deviceParamsKey;
+                    } else {
+                        VIAM_SDK_LOG(info) << "Created value " << valueName << " = " << value << " for key " << deviceParamsKey;
+                    }
                 } else if (result == ERROR_SUCCESS) {
                     VIAM_SDK_LOG(info) << "Value already exists on key " << deviceParamsKey << ", skipping.";
                 } else {
                     VIAM_SDK_LOG(error) << "Error reading metadata value for key " << deviceParamsKey << ": " << result;
-            }
-            RegCloseKey(hDeviceKey);
-            ++index;
+                }
+                RegCloseKey(hDeviceKey);
+                ++index;
             }
             RegCloseKey(hkey);
-    }
+        }
     } catch (const std::exception& e) {
-        //throw std::runtime_error("GOT EXCEPTION!");
-        VIAM_SDK_LOG(error) << "error in windows " << e.what();
-        throw std::runtime_error("EXCEPTION" + std::string(e.what()));
+        throw std::runtime_error("failed to update windows device registry keys: " + std::string(e.what()));
     }
 #endif
     {
